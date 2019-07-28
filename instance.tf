26d95b62a6f8e3cb44aab86898432c6a643e8577
apenjiyev <azatpenji@gmail.com>
2019-07-28 15:02:03 -0700

    latest

--- instance.tf
+++ instance.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web1" {
     ami = "ami-0b898040803850657"
     instance_type = "t2.micro"
     key_name = "${aws_key_pair.terraform}"
+    security_groups = [${aws_security_group.sec1.id}]
     
     tags = {
         Name = "Dev"
