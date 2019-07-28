95e2a53bf7299cbd58655fb1da50597518b185f3
apenjiyev <azatpenji@gmail.com>
2019-07-28 14:58:25 -0700

    key pair paired now

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web1" {
     ami = "ami-0b898040803850657"
     instance_type = "t2.micro"
-    key_name = "terraform"
+    key_name = "${aws_key_pair.terraform}"
     
     tags = {
         Name = "Dev"
