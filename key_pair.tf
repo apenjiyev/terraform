4bec18159f0557fbc5ebeae9b963659072a33e91
apenjiyev <azatpenji@gmail.com>
2019-07-28 14:18:33 -0700

    added

--- key_pair.tf
+++ key_pair.tf
@@ -1,5 +1,4 @@
 resource "aws_key_pair" "terraform" {
   key_name   = "terraform"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAeZZ9nSwPw+1jTIMpwXFuXyYsAvRkuBCXO3RWGwi64utqfV2OG7ruqMZhFcQbYnwERTvhq1MYwg2oEZX1/AfSRmc3DvNcttNqN+/nHe1MPzcDibTrgf/URD3+Dch6BWRGHPbbOlQODWDkHnAG6KQp65y6BEaOzT8VreQ1MBb6w0uzg3cfrzz4cKsFMmSxkph/+2xO3REQf6kIreQbnnpvqQyB32/GyMB5/ZOMn+nkYMqJhpQkuNaW7yYuDE9qkZZ5Y9RqPU34OQ8UEnTBLbIz5K/M1pniCaBgoeGZoQYWMEjfHG/Y334yBHwuoJa9gLRTxpVTHrMVz3B+/dEHIS+p root@ip-172-31-82-151.ec2.internal
-"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAeZZ9nSwPw+1jTIMpwXFuXyYsAvRkuBCXO3RWGwi64utqfV2OG7ruqMZhFcQbYnwERTvhq1MYwg2oEZX1/AfSRmc3DvNcttNqN+/nHe1MPzcDibTrgf/URD3+Dch6BWRGHPbbOlQODWDkHnAG6KQp65y6BEaOzT8VreQ1MBb6w0uzg3cfrzz4cKsFMmSxkph/+2xO3REQf6kIreQbnnpvqQyB32/GyMB5/ZOMn+nkYMqJhpQkuNaW7yYuDE9qkZZ5Y9RqPU34OQ8UEnTBLbIz5K/M1pniCaBgoeGZoQYWMEjfHG/Y334yBHwuoJa9gLRTxpVTHrMVz3B+/dEHIS+p root@ip-172-31-82-151.ec2.internal"
 }
