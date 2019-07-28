2b0d751b9bf7683b64801edbcc931de6c644a8fc
apenjiyev <azatpenji@gmail.com>
2019-07-28 14:33:25 -0700

    added sc

---
+++ sec_group.tf
@@ -0,0 +1,12 @@
+resource "aws_security_group" "sec1" {
+  name        = "sec1"
+  description = "Allow ssh"
+  vpc_id      = "vpc-f469098e"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "-1"
+    cidr_blocks = "0.0.0.0/0"
+  }
+}
